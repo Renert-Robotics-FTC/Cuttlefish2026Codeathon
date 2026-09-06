fa946fb14529bc33bf92d0d95b8eefd4399358f6
Arjun Gill <arjungill@si-iphone.localdomain>
2026-09-06 14:44:00 -0600

    Added robot centric drive and used method in teleop.java, may or may not add field centric if time permits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveSubsystem.java
@@ -2,17 +2,49 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class DriveSubsystem {
-    final DcMotor backleftmotor;
-    final DcMotor backrightmotor;
-    final DcMotor frontrightmotor;
-    final DcMotor frontleftmotor;
+    final DcMotor FR;
+    final DcMotor BR;
+    final DcMotor FL;
+    final DcMotor BL;
+
+
+    private double straight;
+    private double turn;
+    private double strafe;
 
     public DriveSubsystem(HardwareMap hardwareMap) {
-        backleftmotor = hardwareMap.get(DcMotor.class, "BL");
-        backrightmotor = hardwareMap.get(DcMotor.class,"BR");
-        frontrightmotor = hardwareMap.get(DcMotor.class,"FR");
-        frontleftmotor = hardwareMap.get(DcMotor.class,"FL");
+        FR = hardwareMap.get(DcMotor.class, "FR");
+        BR = hardwareMap.get(DcMotor.class, "BR");
+        FL = hardwareMap.get(DcMotor.class, "FL");
+        BL = hardwareMap.get(DcMotor.class, "BL");
+
+        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        BL.setDirection(DcMotor.Direction.REVERSE);
+        FL.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void updateInputs(double newStraight, double newStrafe, double newTurn) {
+        straight = newStraight;
+        strafe = newStrafe;
+        turn = newTurn;
+
+    }
+    public void update() {
+        double bottomnumber = Math.max(
+                1.0,
+                Math.abs(straight) + Math.abs(strafe) + Math.abs(turn)
+        );
+        double speedcapfactor = 1.0 / bottomnumber;
+        BL.setPower((straight + strafe + turn) * speedcapfactor);
+        FL.setPower((straight - strafe + turn) * speedcapfactor);
+        BR.setPower((straight - strafe - turn) * speedcapfactor);
+        FR.setPower((straight + strafe - turn) * speedcapfactor);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,23 +1,24 @@
 package org.firstinspires.ftc.teamcode;
-//imports
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-@TeleOp(name = "Main Teleop")
-public class Teleop extends LinearOpMode{
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Main Teleop")
+public class Teleop extends LinearOpMode {
 
     @Override
-    public void runOpMode(){
-        //create subsystem objects here
+    public void runOpMode() {
+        DriveSubsystem driveSubsystem = new DriveSubsystem(hardwareMap);
 
         waitForStart();
 
         while (opModeIsActive()) {
-            //put loop code here
+            double straight = gamepad1.left_stick_y;
+           double strafe = gamepad1.left_stick_x;
+           double turn = gamepad1.right_stick_x;
+
+            driveSubsystem.updateInputs(straight, strafe, turn);
+            driveSubsystem.update();
         }
-
     }
-
-
 }
