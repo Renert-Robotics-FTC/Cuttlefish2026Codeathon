fae52fb5fa856ff7939bd57c7fc1b73b8f097500
Arjun Gill <arjungill@si-iphone.localdomain>
2026-09-07 11:57:36 -0600

    added field centric and proper coding conventions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveSubsystem.java
@@ -2,12 +2,16 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class DriveSubsystem {
     final DcMotor FR;
     final DcMotor BR;
     final DcMotor FL;
     final DcMotor BL;
+
+    final GoBildaPinpointDriver pinpoint;
 
 
     private double straight;
@@ -16,17 +20,21 @@ public class DriveSubsystem {
 
     public DriveSubsystem(HardwareMap hardwareMap) {
         FR = hardwareMap.get(DcMotor.class, "FR");
-        BR = hardwareMap.get(DcMotor.class, "BR");
-        FL = hardwareMap.get(DcMotor.class, "FL");
-        BL = hardwareMap.get(DcMotor.class, "BL");
+           BR = hardwareMap.get(DcMotor.class, "BR");
+            FL = hardwareMap.get(DcMotor.class, "FL");
+                BL = hardwareMap.get(DcMotor.class, "BL");
+        // sensor that tracks robots position and direction
+                pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+        // reset pinpoint position and calibrate heading while the robot is not moving
+        pinpoint.resetPosAndIMU();
 
         FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+            BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //reverse left motors bc they are opposite right motors
         BL.setDirection(DcMotor.Direction.REVERSE);
-        FL.setDirection(DcMotor.Direction.REVERSE);
+         FL.setDirection(DcMotor.Direction.REVERSE);
     }
 
     public void updateInputs(double newStraight, double newStrafe, double newTurn) {
@@ -36,15 +44,25 @@ public class DriveSubsystem {
 
     }
     public void update() {
+       //refresh pinpoint odo data before reading robot heading
+        pinpoint.update();
+        //used ai to help with this line, kept getting error and couldnt figure it out
+        double heading = pinpoint.getPosition().getHeading(AngleUnit.RADIANS);
+
+        double robotStraight = straight * Math.cos(heading) - strafe * Math.sin(heading);
+        double robotStrafe = straight * Math.sin(heading) + strafe * Math.cos(heading);
+        //keep motor power between -1 and 1
         double bottomnumber = Math.max(
                 1.0,
-                Math.abs(straight) + Math.abs(strafe) + Math.abs(turn)
+                Math.abs(robotStraight) + Math.abs(robotStrafe) + Math.abs(turn)
         );
         double speedcapfactor = 1.0 / bottomnumber;
-        BL.setPower((straight + strafe + turn) * speedcapfactor);
-        FL.setPower((straight - strafe + turn) * speedcapfactor);
-        BR.setPower((straight - strafe - turn) * speedcapfactor);
-        FR.setPower((straight + strafe - turn) * speedcapfactor);
+        //Convert the driver's field direction into the robot's current direction.
+        BL.setPower((robotStraight + robotStrafe + turn) * speedcapfactor);
+        FL.setPower((robotStraight - robotStrafe + turn) * speedcapfactor);
+        BR.setPower((robotStraight - robotStrafe - turn) * speedcapfactor);
+        FR.setPower((robotStraight + robotStrafe - turn) * speedcapfactor);
+
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
+// Controls: Left Stick Y is forward and backward
+// Left stick X, is left and right strafe
+// Right stick X is robot rotation
 @TeleOp(name = "Main Teleop")
 public class Teleop extends LinearOpMode {
 
@@ -13,9 +15,9 @@ public class Teleop extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            double straight = gamepad1.left_stick_y;
-           double strafe = gamepad1.left_stick_x;
-           double turn = gamepad1.right_stick_x;
+            double straight = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
 
             driveSubsystem.updateInputs(straight, strafe, turn);
             driveSubsystem.update();
