f682ad9d5575cca7c4abf0c1bfe49de83d2360dd
Lucas Cheung <lucascheung@Lucass-MacBook-Air-3.local>
2026-09-08 19:23:37 -0600

    Indexer subsytem coded, with gamepad button a turning on the indexer motor, and turning it off when the button is released.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -4,17 +4,26 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Main Teleop")
-public class Teleop extends LinearOpMode{
+public class Teleop extends LinearOpMode {
 
-
+boolean indexing = false;
     @Override
     public void runOpMode(){
         //create subsystem objects here
-
+        indexerspinning indexer = new indexerspinning(hardwareMap);
         waitForStart();
 
         while (opModeIsActive()) {
-            //put loop code here
+            gamepad1.a = indexing;
+            if (indexing){
+                indexer.indexerspin1();
+
+
+            } else{
+                indexer.indexerspin0();
+            }
+
+
         }
 
     }
