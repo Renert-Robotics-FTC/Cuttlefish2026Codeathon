787bb48bc9cf3ce241249d6c8578d3599ea7573a
haochenwangpoke <haochen.wang.poke@gmail.com>
2026-09-09 12:04:30 -0600

    Add comment for ShooterSubsystem constants
    Added a comment to describe the purpose of the constants.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConstantFile.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-
+//Constants that will be used in ShooterSubsystem 
 public class ConstantFile {
     public static class ShooterConstants{
         public static final double powerForward = 0.75;
