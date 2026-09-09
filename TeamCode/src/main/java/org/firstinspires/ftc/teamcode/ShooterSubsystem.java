2929cd6cd9103c2bd5a3ccf75f3a83812a28b5b8
haochenwangpoke <haochen.wang.poke@gmail.com>
2026-09-09 11:55:15 -0600

    Enhance comments in ShooterSubsystem class
    Added comments for clarity on shooter motor functionality.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterSubsystem.java
@@ -4,12 +4,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ShooterSubsystem {
-    public DcMotor shooterMotor;//The motor that is needed to spin
-
+    //The motor that is needed to spin  
+    public DcMotor shooterMotor;
+    
+    // HardwareMap lets us find the physical motor connected to the robot.
     public ShooterSubsystem(HardwareMap hardwareMap){
         shooterMotor = hardwareMap.get(DcMotor.class,"shooter");
     }
-
+     // This method controls the direction of the shooter.
+    //
+    // forward = true  -> spin the shooter forward
+    // back = true     -> spin the shooter backward
+    // both false      -> stop the shooter
+    //
+    // We use booleans (true/false) to tell the subsystem what
+    // the driver wants the shooter to do.
     public void shoot(boolean forward, boolean back){
         if(forward){
             shooterMotor.setPower(ConstantFile.ShooterConstants.powerForward);
